d45f1bcca6e5c655a370cc9cb4f3e10ff48cb275
Jane <m3281@hotmail.com>
2020-09-16 17:42:51 +0800

    1.35

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,28 @@ class UsersController < ApplicationController
 
     def payment
         @fee = ENV["price_#{params[:type]}"]
-        @payment_type = (params[:type] == 'vip')? 'VIP會員' : '白金會員'
+        @payment_type = (params[:type] == 'vip') ? 'VIP會員' : '白金會員'
         @token = gateway.client_token.generate
+    end
+
+    def pay
+        fee = ENV["price_#{params[:type]}"]
+        nonce = params[:payment_nonce]
+
+        result = gateway.transaction.sale(
+            amount: "10.00",
+            payment_method_nonce: nonce,
+            options: {
+            submit_for_settlement: true
+            }
+        )
+
+        if result.success?
+            current_user.send("#{params[:type]}_user!")
+            redirect_to root_path, notice: '付費成功'
+        else
+            redirect_to root_path, notice: '付費發生錯誤'
+        end
     end
 
     private
